dc45307f96387995d219f16b08ba0eead6e55794
ChalankaKodikarae <ischalankakodikara@gmail.com>
2023-12-10 12:21:57 +0530

    All files Added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import FeedbackForm from "./components/FeedbackForm";
+import FeedbackCard from "./components/FeedbackCard";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FeedbackForm />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+        <FeedbackCard />
     </div>
   );
 }
